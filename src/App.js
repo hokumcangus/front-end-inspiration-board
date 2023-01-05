14dc615d3d599c5dfd43b3560c6fdc7b2224561c
Anika Stephen Wilbur <anika.m.stephen@gmail.com>
2023-01-05 09:52:06 -0800

    Started working on styling

--- src/App.js
+++ src/App.js
@@ -1,132 +1,159 @@
-import './App.css';
-import React from 'react';
-import {useEffect, useState} from 'react';
-import axios from 'axios';
-import CardsList from './components/CardsList';
-import NewBoardForm from './components/NewBoardForm';
-import Board from './components/Board';
+import "./styles/App.css";
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import CardsList from "./components/CardsList";
+import NewBoardForm from "./components/NewBoardForm";
+import Board from "./components/Board";
 
 function App() {
-  const [showAddCard, setShowAddCard] = useState([]);
-  const [cards, setCards] = useState([
-    {
-      'cardId': 1,
-      'message': 'Good day'
-    },
-    {
-      'cardId': 2,
-      'message': 'It is a beautiful day'
-    }
-  ]);
+  // const [showAddCard, setShowAddCard] = useState([]);
+  // const [cards, setCards] = useState([
+  //   {
+  //     cardId: 1,
+  //     message: "Good day",
+  //   },
+  //   {
+  //     cardId: 2,
+  //     message: "It is a beautiful day",
+  //   },
+  // ]);
 
   const [boardsData, setBoardsData] = useState([
     {
-      'boardId': 1,
-      'title': 'Board 1',
-      'owner': 'Hoku & Anika'
+      boardId: 1,
+      title: "Board 1",
+      owner: "Hoku & Anika",
     },
     {
-      'boardId': 2,
-      'title': 'Board 2',
-      'owner': 'Alaere & Mia'
-    }
+      boardId: 2,
+      title: "Board 2",
+      owner: "Alaere & Mia",
+    },
   ]);
-
-
-  
 
   // const addCard = (card) => {
   //   setCards([...cards, card])
   // };
 
-
-
-  
   // const [boardsData, setBoardsData] = useState([]);
   const [selectedBoard, setSelectedBoard] = useState({
-    title: '',
-    owner: '',
-    board_id: null
+    title: "",
+    owner: "",
+    boardId: null,
   });
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_BACKEND_URL}/boards`, {
-    }).then((response) => {
-      setBoardsData(response.data);
-    })
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/boards`, {})
+      .then((response) => {
+        setBoardsData(response.data);
+      });
   }, []);
 
-  const selectBoard = (board) => { setSelectedBoard(board) };
+  const selectBoard = (board) => {
+    setSelectedBoard(board);
+  };
 
   const boardsElements = boardsData.map((board) => {
-    return (<li>
-      <Board board={board} onBoardSelect={selectBoard}></Board>
-    </li>)
+    return (
+      <li>
+        <Board board={board} onBoardSelect={selectBoard}></Board>
+      </li>
+    );
   });
 
   const createNewBoard = (newBoard) => {
-    axios.post(`${process.env.REACT_APP_BACKEND_URL}/boards`, newBoard).then((response) => {
-      console.log("Response:", response.data.board);
-      const boards = [...boardsData];
-      boards.push(response.data.board);
-      setBoardsData(boards);
-    }).catch((error) => {
-      console.log('Error:', error);
-      alert('Couldn\'t create a new board.');
-    });
-  }
+    axios
+      .post(`${process.env.REACT_APP_BACKEND_URL}/boards`, newBoard)
+      .then((response) => {
+        console.log("Response:", response.data.board);
+        const boards = [...boardsData];
+        boards.push(response.data.board);
+        setBoardsData(boards);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+        alert("Couldn't create a new board.");
+      });
+  };
 
   const [isBoardFormVisible, setIsBoardFormVisible] = useState(true);
-  const toggleNewBoardForm = () => {setIsBoardFormVisible(!isBoardFormVisible)}
+  const toggleNewBoardForm = () => {
+    setIsBoardFormVisible(!isBoardFormVisible);
+  };
 
-  
-  
-  const deleteAll = () => {
-    if (window.confirm('Are you really sure? Please be gentle with this demo.')) {
-      axios.delete(`${process.env.REACT_APP_BACKEND_URL}/destroy_all`).then((response) => {
-        console.log('response', response.data);
-        setBoardsData([response.data.default_board]);
-        setSelectedBoard({
-          title: '',
-          owner: '',
-          board_id: null
-        });
-      }).catch((error) => {
-        console.log('Error:', error);
-        alert('Something went wrong! :(');
-      });
-    }
-  }
+  // const deleteAll = () => {
+  //   if (
+  //     window.confirm("Are you really sure? Please be gentle with this demo.")
+  //   ) {
+  //     axios
+  //       .delete(`${process.env.REACT_APP_BACKEND_URL}/destroy_all`)
+  //       .then((response) => {
+  //         console.log("response", response.data);
+  //         setBoardsData([response.data.default_board]);
+  //         setSelectedBoard({
+  //           title: "",
+  //           owner: "",
+  //           board_id: null,
+  //         });
+  //       })
+  //       .catch((error) => {
+  //         console.log("Error:", error);
+  //         alert("Something went wrong! :(");
+  //       });
+  //   }
+  // };
 
   return (
-    <div className="page__container">
-      <header></header>
-      <body className="content__container">
-        <section> 
-          <h1>2023 Inspiration Board</h1>
-          <section className="boards__container">
-          {/* </section> */}
-            <h2>Boards</h2>
-            <ol className="boards__list">
-              {boardsElements}
-            </ol>
+    <div className="parentWrap">
+      <header>
+        <h1>Inspiration Board</h1>
+      </header>
+      <body>
+        <section className="flexbox">
+          <section>
+            <h2>List of Boards</h2>
+            <p>Select a board from the list below</p>
+            <ul className="listOfBoards">{boardsElements}</ul>
           </section>
           <section>
-            <h2>Selected Board</h2>
-            <p>{selectedBoard.board_id ? `${selectedBoard.title} - ${selectedBoard.owner}` : 'Select a Board from the Board List!'}</p>
+            <h2>Now Viewing:</h2>
+            <p>
+              {selectedBoard.boardId
+                ? `${selectedBoard.title} - ${selectedBoard.owner}`
+                : ""}
+            </p>
           </section>
-          <section className='new-board-form__container'>
-            <h2>Create a New Board</h2>
-            {isBoardFormVisible ? <NewBoardForm createNewBoard={createNewBoard}></NewBoardForm> : ''}
-            <span onClick={toggleNewBoardForm} className='new-board-form__toggle-btn'>{isBoardFormVisible ? 'Hide New Board Form' : 'Show New Board Form'}</span>
+          <section>
+            <h2>Need a New Board?</h2>
+            <p>Enter details below and click Create</p>
+            {isBoardFormVisible ? (
+              <NewBoardForm createNewBoard={createNewBoard}></NewBoardForm>
+            ) : (
+              ""
+            )}
+            <span onClick={toggleNewBoardForm} className="toggleNewBoardForm">
+              {isBoardFormVisible
+                ? "Hide New Board Form"
+                : "Show New Board Form"}
+            </span>
           </section>
         </section>
-        {selectedBoard.board_id ? <CardsList board={selectedBoard}></CardsList> : ''}
+        {selectedBoard.board_id ? (
+          <CardsList board={selectedBoard}></CardsList>
+        ) : (
+          ""
+        )}
       </body>
-      <footer><span></span> Click <span onClick={deleteAll} className="footer__delete-btn">here</span> to delete all boards and cards!</footer>
+      <footer>
+        <h3>
+          © 2023 Inspiration Board | Hoku, Alaere, Anika, & Mia | C18 Snow
+          Leopards
+        </h3>
+      </footer>
     </div>
   );
 }
-
 
 export default App;
--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,39 +1,65 @@
-import { useState } from 'react';
+import "../styles/NewBoardForm.css";
+import { useState } from "react";
 
 const NewBoardForm = (props) => {
-
-  const [title, setTitle] = useState('');
-  const [owner, setOwner] = useState('');
-  const handleTitleChange = (e) => { setTitle(e.target.value) };
-  const handleOwnerChange = (e) => { setOwner(e.target.value) };
-
-  const submitNewBoard = (e) => {
-    e.preventDefault();
-    props.createNewBoard({ title, owner });
-    setTitle('');
-    setOwner('');
+  const [title, setTitle] = useState("");
+  const [owner, setOwner] = useState("");
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleOwnerChange = (e) => {
+    setOwner(e.target.value);
   };
 
+  const createNewBoard = (e) => {
+    e.preventDefault();
+    props.createNewBoard({ title, owner });
+    setTitle("");
+    setOwner("");
+  };
 
-  return (<form onSubmit={submitNewBoard} className='new-board-form__form'>
-        <label>Title</label>
-        <input
-          type="text"
-          value={title}
-          onChange={handleTitleChange}
-          className={((title.length === 0) || (title.length > 40)) ? 'invalid-form-input' : ''}></input>
-        <label>Owner's Name</label>
-        <input
-          type="text"
-          value={owner}
-          onChange={handleOwnerChange}
-          className={((owner.length === 0) || (owner.length > 40)) ? 'invalid-form-input' : ''}></input>
-        <p>Preview: {title} - {owner}</p>
-        <input
-          type="Submit"
-          disabled={((title.length === 0) || (owner.length === 0) || (title.length > 40) || (owner.length > 40))}
-          className='new-board-form__form-submit-btn'></input>
-      </form>);
-}
+  return (
+    <form onSubmit={createNewBoard} className="newBoardForm">
+      <label>Title</label>
+      <br />
+      <input
+        type="text"
+        value={title}
+        onChange={handleTitleChange}
+        className={
+          title.length === 0 || title.length > 40 ? "invalidInput" : ""
+        }
+      ></input>
+      <br />
+      <br />
+      <label>Owned By</label>
+      <br />
+      <input
+        type="text"
+        value={owner}
+        onChange={handleOwnerChange}
+        className={
+          owner.length === 0 || owner.length > 40 ? "invalidInput" : ""
+        }
+      ></input>
+      <p>
+        Preview: {title} - {owner}
+      </p>
+      {/* <input
+        type="Submit"
+        disabled={
+          title.length === 0 ||
+          owner.length === 0 ||
+          title.length > 40 ||
+          owner.length > 40
+        }
+        className="new-board-form__form-submit-btn"
+      ></input> */}
+      <button type="Submit" className="realTime">
+        Create
+      </button>
+    </form>
+  );
+};
 
 export default NewBoardForm;
