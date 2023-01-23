921340ad6ce76b15348e59b3eee291f068504199
Anika Stephen Wilbur <anika.m.stephen@gmail.com>
2023-01-23 00:18:57 -0800

    The formatting in GitHub was looking funny. Trying to fix

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ const kBaseUrl = process.env.REACT_APP_BE_URL;
 
 function App() {
 
-	const [boardsData, setBoardsData] = useState([]);
+  const [boardsData, setBoardsData] = useState([]);
   const [cardsData, setCardsData] = useState([]);
-	const [selectedBoard, setSelectedBoard] = useState({
-		title: "",
-		owner: "",
-		board_id: null,
-	});
+  const [selectedBoard, setSelectedBoard] = useState({
+    title: "",
+    owner: "",
+    board_id: null,
+  });
   const [isBoardFormShowing, setIsBoardFormShowing] = useState(true);
 
   const selectBoard = (board) => {
-		setSelectedBoard(board);
-	};
+    setSelectedBoard(board);
+  };
 
   const resetSelectBoard = () => {
     setSelectedBoard(
@@ -33,61 +33,59 @@ function App() {
   };
 
   const toggleNewBoardForm = () => {
-		setIsBoardFormShowing(!isBoardFormShowing);
-	};  
+    setIsBoardFormShowing(!isBoardFormShowing);
+  };  
 
-	useEffect(() => {
-		axios
-			.get(`${kBaseUrl}/boards`, {})
-			.then((response) => {
+  useEffect(() => {
+    axios
+      .get(`${kBaseUrl}/boards`, {})
+      .then((response) => {
         setBoardsData(response.data);
-			});
-	}, []);
+    });
+  }, []);
 
   const getBoardCards = (board_id) => {
-    console.log("board_id", board_id);
-		axios
-			.get(
-				`${kBaseUrl}/boards/${board_id}/cards`
-			)
-			.then((response) => {
-				setCardsData(response.data.cards);
-			})
-			.catch((error) => {
-				console.log("Error:", error);
-				alert("Unable to retrieve cards for this board");
-			});
-	};
-
-	const boardsList = boardsData.map((board, index) => {
-		return (
-			<li key={index}>
-				<Board
-					board={board}
-					onSelect={selectBoard}
-          displayCards={getBoardCards}
-				></Board>
-			</li>
-		);
-	});
-
-	const createNewBoard = (newBoard) => {
-    console.log("new board", newBoard)
     axios
-			.post(
-				`${kBaseUrl}/boards`, newBoard
-        )
-			.then((response) => {
-				console.log("Response:", response.data.boards);
-				const boards = [...boardsData];
-				boards.push(response.data.boards);
-				setBoardsData(boards);
-			})
-			.catch((error) => {
-				console.log("Error:", error);
-				alert("Error creating a new board");
-			});
-	};
+      .get(
+        `${kBaseUrl}/boards/${board_id}/cards`
+      )
+      .then((response) => {
+        setCardsData(response.data.cards);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+        alert("Unable to retrieve cards for this board");
+    });
+  };
+
+  const boardsList = boardsData.map((board, index) => {
+    return (
+      <li key={index}>
+        <Board
+          board={board}
+          onSelect={selectBoard}
+          displayCards={getBoardCards}
+        ></Board>
+      </li>
+    );
+  });
+
+  const createNewBoard = (newBoard) => {
+    axios
+      .post(
+        `${kBaseUrl}/boards`, newBoard
+      )
+      .then((response) => {
+        console.log("Response:", response.data.boards);
+        const boards = [...boardsData];
+        boards.push(response.data.boards);
+        setBoardsData(boards);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+        alert("Error creating a new board");
+    });
+  };
 
   const deleteOneBoard = (board_id) => {
     axios
@@ -123,125 +121,124 @@ function App() {
   };
 
   const createNewCard = (message) => {
-		axios
-			.post(
-				`${kBaseUrl}/boards/${selectedBoard.board_id}/cards`,
-				{ message }
-			)
-			.then((response) => {
+    axios
+      .post(
+        `${kBaseUrl}/boards/${selectedBoard.board_id}/cards`,
+        { message }
+      )
+      .then((response) => {
         console.log("Response:", response.data.cards);
         const cards = [...cardsData];
-				cards.push(response.data.cards);
-				setCardsData(cards);
-			})
-			.catch((error) => {
-				console.log("Error:", error);
-				alert("Couldn't create a new card.");
-			});
-	};
+        cards.push(response.data.cards);
+        setCardsData(cards);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+        alert("Couldn't create a new card.");
+    });
+  };
 
   const deleteCard = (cardId) => {
     axios
-			.delete(
-				`${kBaseUrl}/cards/${cardId}`
-			)
-			.then((response) => {
+      .delete(
+        `${kBaseUrl}/cards/${cardId}`
+      )
+      .then((response) => {
         const newCardsData = cardsData.filter((deletedCard) => {
-					return deletedCard.card_id !== cardId;
-				});
-				setCardsData(newCardsData);
-			})
-			.catch((error) => {
-				console.log("Error:", error);
-				alert("Unable to delete the selected card");
-			});
-	};
+          return deletedCard.card_id !== cardId;
+        });
+        setCardsData(newCardsData);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+        alert("Unable to delete the selected card");
+    });
+  };
 
   const updateLikes = async (cardId) => {
     try {
-        const res = await axios.patch(`${kBaseUrl}/boards/${selectedBoard.board_id}/cards/${cardId}`);
-        onCardLikes(cardId);
+      const res = await axios.patch(`${kBaseUrl}/boards/${selectedBoard.board_id}/cards/${cardId}`);
+      onCardLikes(cardId);
     } catch(err) {
-        console.error(err);
+      console.error(err);
     }
-}
+  }
 
-const onCardLikes = (cardId) => {
-  setCardsData(cardsData.map(card => {
-    if(card.card_id === cardId) {
-        return {...card, likes: card.likes + 1}
-    } else {
-        return card;
-    }
-}));
-}
+  const onCardLikes = (cardId) => {
+    setCardsData(cardsData.map(card => {
+      if(card.card_id === cardId) {
+          return {...card, likes: card.likes + 1}
+      } else {
+          return card;
+      }
+  }));
+  }
 
-	return (
-		<main className="mainContainer">
-			<section>
-				<h1>Inspiration Board</h1>
-			</section>
-			<section>
-				<section className="upperGrid">
-					<section>
-						<h2 className="noBottomMargin">List of Boards</h2>
+  return (
+    <main className="mainContainer">
+      <section>
+        <h1>Inspiration Board</h1>
+      </section>
+      <section>
+        <section className="upperGrid">
+          <section>
+            <h2 className="noBottomMargin">List of Boards</h2>
             <div className="deleteBoards"
               onClick={(event) => deleteAllBoards(boardsData)}>
-                Delete All Boards
+              Delete All Boards
             </div>
-						<p>Select a board to view from the list below</p>
-						<ul className="listOfBoards">{boardsList}</ul>
-					</section>
-					<section>
-						<h2 className="noBottomMargin">Now Viewing:</h2>
+            <p>Select a board to view from the list below</p>
+            <ul className="listOfBoards">{boardsList}</ul>
+          </section>
+          <section>
+            <h2 className="noBottomMargin">Now Viewing:</h2>
             <div className="deleteBoards"
               onClick={(event) => deleteOneBoard(selectedBoard.board_id)}>
               {selectedBoard.board_id 
-                  ? "Delete This Board"
-                  : ""}
+                ? "Delete This Board"
+                : ""}
             </div>
-						<p className="selectedBoard">
-							{selectedBoard.board_id &&
-								`${selectedBoard.title} by ${selectedBoard.owner}`}
+            <p className="selectedBoard">
+              {selectedBoard.board_id &&
+              `${selectedBoard.title} by ${selectedBoard.owner}`}
             </p>
-					</section>
-					<section>
-						<h2>Need a New Board?</h2>
-						<p>Enter details below and click Create</p>
-						{isBoardFormShowing && (
-							<NewBoardForm
-								addNewBoard={createNewBoard}
-							></NewBoardForm>
-						)}
-						<br />
-						<div
-							onClick={toggleNewBoardForm}
-							className="toggleNewBoardForm"
-						>
-							{isBoardFormShowing
-								? "Hide New Board Form"
-								: "Show New Board Form"}
-						</div>
-					</section>
-				</section>
-				{selectedBoard.board_id && (
-					<CardsList 
-          board={selectedBoard}
-          onUpdateLikes={updateLikes}
-          onDeleteCard={deleteCard}
-          addNewCard={createNewCard}
-          cards={cardsData}
+          </section>
+          <section>
+            <h2>Need a New Board?</h2>
+            <p>Enter details below and click Create</p>
+              {isBoardFormShowing && (
+                <NewBoardForm
+                addNewBoard={createNewBoard}
+                ></NewBoardForm>
+              )}
+            <br />
+            <div
+              onClick={toggleNewBoardForm}
+              className="toggleNewBoardForm">
+              {isBoardFormShowing
+                ? "Hide New Board Form"
+                : "Show New Board Form"}
+            </div>
+          </section>
+        </section>
+        {selectedBoard.board_id && (
+          <CardsList 
+            board={selectedBoard}
+            onUpdateLikes={updateLikes}
+            onDeleteCard={deleteCard}
+            addNewCard={createNewCard}
+            cards={cardsData}
           ></CardsList>
-				)}
-			</section>
-			<footer>
-				<h3>
-					© 2023 Inspiration Board | Hoku, Alaere, Anika, & Mia | C18
-					Snow Leopards
-				</h3>
-			</footer>
-		</main>
-	);
-}
+        )}
+      </section>
+      <footer>
+        <h3>
+          © 2023 Inspiration Board | Hoku, Alaere, Anika, & Mia | C18
+          Snow Leopards
+        </h3>
+      </footer>
+    </main>
+  );
+};
 
 export default App;
